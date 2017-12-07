42758a72248d0e5c6c3678f109677b5cec651b19
Tony Luo <luo.tony37@gmail.com>
2017-12-06 16:07:36 -0800

    refactor: Comment components.

--- frontend/src/components/CommentForm.js
+++ frontend/src/components/CommentForm.js
@@ -1,15 +1,111 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createComment, editComment } from '../actions';
+import { Redirect } from 'react-router-dom';
+import shortid from 'shortid';
 
 class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    if (props.location.state.comment) {
+      this.state = {
+        ...props.location.state.comment,
+        editing: true
+      }
+    }
+     else {
+       this.state = {
+         author: props.author || '',
+         body: props.body || '',
+         postId: props.location.state.postId,
+         shouldRedirect: false,
+         editing: false
+       }
+     }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const comment = {
+      id: this.state.id || shortid.generate(),
+      parentId: this.state.parentId || this.state.postId,
+      timestamp: this.state.timestamp || Date.now(),
+      author: this.state.author,
+      body: this.state.body
+    }
+
+    this.state.editing ? this.props.editComment(comment) : this.props.createComment(comment);
+
+    this.setState({
+      shouldRedirect: true
+    })
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
+    if (this.state.shouldRedirect && !this.state.editing) {
+      return <Redirect to={`/category/${this.props.location.state.postId}`} />
+    }
+    if (this.state.shouldRedirect && this.state.editing) {
+      return <Redirect to={`/category/${this.state.parentId}`} />
+    }
     return (
-      <form>
-        <input type="text" placeholder="Author"/>
-        <textarea placeholder="comment body"></textarea>
-        <button>Submit comment</button>
-      </form>
+      <div
+        className="form-container">
+        <form
+          className="form"
+          onSubmit={this.handleSubmit}>
+          <input
+            className="form__input"
+            type="text"
+            value={this.state.author}
+            onChange={this.handleChange}
+            name="author"
+            placeholder="Author"
+          />
+          <textarea
+            className="form__textarea"
+            placeholder="comment body"
+            name="body"
+            onChange={this.handleChange}
+            value={this.state.body}
+          />
+          <input
+            className="form__button"
+            type="submit"
+            value={this.state.editing ? 'Edit Comment' : 'Submit Comment'}
+          />
+        </form>
+      </div>
     )
   }
 }
 
-export default CommentForm;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    comments: state.comments
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createComment: (commentData) => {
+      dispatch(createComment(commentData));
+    },
+    editComment: (commentData) => {
+      dispatch(editComment(commentData))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentForm);
--- frontend/src/components/CommentList.js
+++ frontend/src/components/CommentList.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import VoteScoreStepper from '../containers/VoteScoreStepper';
 
-const export CommentList = (props) => {
+const CommentList = (props) => {
   return (
     <section>
-      <button>Add Comment</button>
-      <ul>
-        <li>What? <button>edit</button><button>delete</button></li>
-        <li>That's so cute!!!! <button>edit</button><button>delete</button></li>
-        <li>Reminds of the time when... <button>edit</button><button>delete</button></li>
+      <ul className="comment-list">
+        {props.comments.filter(comment => !comment.delete).map(comment => {
+          return (
+            <li className="comment" key={comment.id}>
+              <div className="comment__vote-action">
+              <span className="comment__votescore">{comment.voteScore}</span><VoteScoreStepper id={comment.id} />
+              </div>
+              <div className="comment__content">
+                <span className="comment__author">{comment.author}</span> | <span className="comment__body">{comment.body}</span>
+              </div>
+              <div className="comment__actions">
+                <Link className="comment__button" to={{pathname: "/comments/edit", state: {comment}}}>Edit</Link>
+                <a className="comment__button" onClick={() => props.deleteComment(comment.id)}>Delete</a>
+              </div>
+            </li>
+          )
+        })}
       </ul>
+      <Link className="comment__button comment__button--large" to={{pathname: "/comments/new", state: {postId: props.postId}}}>Add Comment</Link>
     </section>
   )
 }
+
+export default CommentList;

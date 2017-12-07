ed7a535e6ddb51630cac99b2973198e02bea8408
Tony Luo <luo.tony37@gmail.com>
2017-12-06 16:06:52 -0800

    refactor: Add root component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,43 @@
-import React, {Component} from 'react';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import React, { Component } from 'react';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import reducer from './reducers';
-import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-
-
+import RootView from './containers/RootView';
 import Header from './components/Header';
-import CategoryList from './components/CategoryList';
-import PostList from './components/PostList';
-import NewPost from './components/NewPost';
-import PostFilter from './components/PostFilter';
+import PostDetail from './components/PostDetail';
+import CommentForm from './components/CommentForm';
+import PostForm from './components/PostForm';
+import NoMatch from './components/NoMatch';
+import './styles/main.css';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header/>
-        <CategoryList/>
-        <PostList/>
-        <PostFilter/>
-        <NewPost/>
-      </div>
+      <Provider store={store}>
+        <div className="main-wrapper">
+          <Router>
+            <div>
+              <Header />
+              <Switch>
+                <Route exact path="/category/:id" component={PostDetail} />
+                <Route exact path="/comments/new" component={CommentForm} />
+                <Route exact path="/comments/edit" component={CommentForm} />
+                <Route exact path="/posts/new" component={PostForm} />
+                <Route exact path="/posts/edit" component={PostForm} />
+                <Route exact path="/:category" component={RootView} />
+                <Route exact path="/" component={RootView} />
+                <Route component={NoMatch} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </Provider>
     );
   }
 }
--- frontend/src/containers/RootView.js
+++ frontend/src/containers/RootView.js
@@ -1,29 +1,68 @@
 import React from 'react';
-import CategoryList from '../components/categorylist';
-import PostList from '../components/postlist';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PostList from '../components/PostList';
+import PostFilter from '../components/PostFilter';
+import {
+  getPosts,
+  getCategories,
+  selectSortFilter
+} from '../actions';
 
 class RootView extends React.Component {
+  componentWillMount() {
+    this.props.getCategories();
+    this.props.getPosts();
+  }
+
   render() {
+    const category = this.props.location.pathname.split('/').pop();
     return (
-      <div>
-        <CategoryList categories={this.props.categories} />
-        <PostList posts={this.props.posts} />
+      <div className="post-wrapper">
+        <Link className="button--circle" to='/posts/new'>
+          <i className="material-icons button-icon">add</i>
+        </Link>
+        <PostFilter onSelectFilter={this.props.sortBy} />
+        <PostList
+          comments={this.props.comments}
+          posts={!category
+            ? this.props.posts
+            : this.props.posts.filter((post) => post.category === category)}
+        />
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return state;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    posts: state.posts.sort((a, b) => {
+      switch(state.sortFilter) {
+        case 'timestamp':
+          return b.timestamp - a.timestamp;
+        default:
+          return b.voteScore - a.voteScore;
+      }
+    }),
+    sortFilter: state.sortFilter,
+    comments: state.comments
+  };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    
+    sortBy: (filter) => {
+      dispatch(selectSortFilter(filter));
+    },
+    getPosts: () => {
+      dispatch(getPosts());
+    },
+    getCategories: () => {
+      dispatch(getCategories());
+    }
   }
 }
 
-const ConnectedRootView = connect(mapStateToProps)(RootView)
+const ConnectedRootView = connect(mapStateToProps, mapDispatchToProps)(RootView)
 
 export default ConnectedRootView;
